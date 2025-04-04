6f4a93e90915610b4659f95ad2993ce7f4a0ac5e
ArinzeCodes <godsbattleaxe.ezeamuzie@redtechlimited.com>
2025-04-04 10:19:49 +0100

    arinzecodesreceipt

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import Receipt from './receipt';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Receipt />
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+// src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use the new 'react-dom/client' API
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement); // Create a root
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
